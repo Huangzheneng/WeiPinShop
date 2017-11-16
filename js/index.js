bb0f1692005abbfe540c2502840478c2b3827f75
Huangzheneng <32388028+Huangzheneng@users.noreply.github.com>
2017-11-16 22:22:31 +0800

    banner

--- js/index.js
+++ js/index.js
@@ -1,74 +1,44 @@
-function Brand(option) {
-    this._init(option);
-    this.render();
-}
-Brand.prototype={
-    constructor:Brand,
-    _init:function (option) {
-        this.parent=option.parent;
-        this.className=option.className || "brand";
-        this.bgi=option.bgi;
-        this.text=option.text;
-        this.tClassName=option.tClassName;
-        this.content=new BrandContent(option.content);
-        this.odiv=document.createElement('div');
+$(function () {
+    slideshow();
 
-        let self = this;
-        this.odiv.onmouseover = function(){
-            self.content.style.display = "block";
-        }
-        this.odiv.onmouseout = function(){
-            self.content.style.display = "none";
-        }
-    },
-    render:function () {
-        this.odiv.style.backgroundImage="url("+this.bgi+")";
-        this.odiv.className=this.className;
-        let tdiv=document.createElement('div');
-        tdiv.innerHTML = this.text;
-        tdiv.className=this.tClassName;
+    function slideshow() {
+        let index=0;
+       $('.bp li').each(function(index , obj){
+           obj.style.backgroundImage = "url(image/rp"+index+".jpg)";
+       });
+        $('.banner').hover(function () {
+            $('.banner > span').stop().fadeIn();
+        },function () {
+            $('.banner > span').stop().fadeOut();
+        });
+        $('.banner span').click(function () {
+            $('.bp li').eq(index).fadeOut();
+            index+=(this.dataset.dir)*1;
+            index = index<0?3:index;
+            index = index>3?0:index;
+            $('.bp li').eq(index).fadeIn();
+            $(".bp-selector + span").stop().animate({
+                "left":index*$('.bp-selector li').outerWidth()+index*4
+            });
 
-        this.odiv.appendChild(tdiv);
-        this.odiv.appendChild(this.content);
-        this.parent.appendChild(this.odiv);
+        })
+        // $(".banner span").eq(0).click(function(){
+        //     $(".bp li").eq(index).fadeOut();
+        //     index++;
+        //     if(index>3){
+        //         index = 0;
+        //     }
+        //     $(".bp li").eq(index).fadeIn();
+        // });
+        // $(".banner span").eq(1).click(function(){
+        //     $(".bp li").eq(index).fadeOut();
+        //     index--;
+        //     if(index<0){
+        //         index = 3;
+        //     }
+        //     $(".bp li").eq(index).fadeIn();
+        // });
 
-    }
-};
+   }
 
-function BrandContent(option) {
-    this._init(option);
-    return this.render();
-}
-BrandContent.prototype={
-    constructor:Brand,
-    _init:function (option) {
-        this.img1=option.img1;
-        this.img2=option.img2;
-        this.img3=option.img3;
-        this.text=option.text;
-        this.className=option.className || "brand-content";
-    },
-    render:function () {
-        let odiv=document.createElement('div');
-        odiv.className=this.className;
-
-
-        let top=document.createElement('div');
-        let bottom=document.createElement('div');
-        bottom.innerHTML = this.text;
-        odiv.appendChild(top);
-        odiv.appendChild(bottom);
-
-        let img1=document.createElement('img');
-        img1.src = this.img1;
-        let img2=document.createElement('img');
-        img2.src = this.img2;
-        let img3=document.createElement('img');
-        img3.src = this.img3;
-        top.appendChild(img1);
-        top.appendChild(img2);
-        top.appendChild(img3);
-
-        return odiv;
-    }
-};
+});
